29faace11a4f190ea3953bc70208118dacb451d4
Navarrete <aaldiitoo@gmail.com>
2022-10-23 17:00:57 -0500

    App Routes initialization

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './router/AppRouter'
 
 const CalendarApp = () => {
   return (
-    <div>CalendarApp</div>
+    <BrowserRouter>
+      <AppRouter/>
+    </BrowserRouter>
   )
 }
 
